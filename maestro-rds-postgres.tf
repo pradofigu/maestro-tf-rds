cb7eb60eb0f313c17079d80a3dae7392f31867bb
Fábio Badaró <fbadaro2@gmail.com>
2023-11-09 09:12:19 -0300

    Add IaC Pipelines (#1)
    * feat: add pipeline
    
    * feat: create rds instance
    
    * fix: adjust pipeline - aws credentials
    
    * fix: adjust pipeline - terraform variables
    
    * fix: adjust pipeline - terraform variables
    
    * fix: adjust pipeline - terraform variables
    
    * fix: adjust pipeline - terraform subnet group
    
    * fix: adjust pipeline - vpc id
    
    * fix: adjust pipeline - postgres version
    
    * fix: adjust pipeline - remove identifier
    
    * feat: adjust pipeline - trigger execution

---
+++ maestro-rds-postgres.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "maestro_rds_sg" {
+  name   = "maestro-rds-sg"
+  vpc_id = "vpc-0207cbc8b0a47452b"
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    bu = "maestro"
+  }
+}
+
+resource "aws_db_parameter_group" "maestro" {
+  name   = "maestro"
+  family = "postgres15"
+
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+}
+
+resource "aws_db_instance" "maestro" {
+  identifier             = "maestro"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 5
+  engine                 = "postgres"
+  engine_version         = "15.3"
+  username               = var.DB_USERNAME
+  password               = var.DB_PASSWORD
+  db_subnet_group_name   = "maestro-rds-sng"
+  vpc_security_group_ids = [aws_security_group.maestro_rds_sg.id]
+  parameter_group_name   = aws_db_parameter_group.maestro.name
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+}
